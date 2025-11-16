f7e0b9c2cef21bbb2c47ccfe1f2951e8bc048841
NathHayes <nathaniel.eldredge@maine.edu>
2025-11-16 15:38:37 -0500

    pre-merge commit for upstream/task-forms

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,19 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [revealed, setRevealed] = useState<boolean>(false);
+
+    const setReveal = () => {
+        setRevealed(!revealed);
+    };
+    return (
+        <div>
+            <div>
+                <Button onClick={setReveal}>
+                    {revealed ? "Hide Answer" : "Reveal Answer"}
+                </Button>
+            </div>
+            {revealed && <p>42</p>}
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -8,9 +8,37 @@ import { Button } from "react-bootstrap";
  * to produce a random integer between 1 and 6 (inclusive).
  */
 export function d6(): number {
-    return 1 + Math.floor(Math.random() * 6);
+    return (1 + Math.floor(Math.random() * 6)) as number;
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeft] = useState<number>(1);
+    const [rightDie, setRight] = useState<number>(2);
+
+    function rollLeft() {
+        setLeft(d6());
+    }
+
+    function rollRight() {
+        setRight(d6());
+    }
+
+    const isEqual = leftDie === rightDie;
+    const isLose = isEqual && leftDie === 1;
+
+    return (
+        <div>
+            <span data-testid="left-die">{leftDie}</span>{" "}
+            <span data-testid="right-die">{rightDie}</span>
+            <div>
+                <Button onClick={rollLeft}>Roll Left</Button>
+                <Button onClick={rollRight}>Roll Right</Button>
+            </div>
+            {isLose ?
+                <div>Lose</div>
+            : isEqual ?
+                <div>Win</div>
+            :   null}
+        </div>
+    );
 }
